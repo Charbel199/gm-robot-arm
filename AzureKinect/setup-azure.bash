1931136992eacd1bf6a816602526b0a2164726cf
George1044 <Georgedaou_35@hotmail.com>
2022-11-03 20:25:01 +0200

    Add dockerfile (K4tools and ROS Melodic Installed)
    WIP ROS for Azure Kinect

---
+++ AzureKinect/setup-azure.bash
@@ -0,0 +1,7 @@
+apt update
+apt-get install software-properties-common -y
+apt install curl -y
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+apt-add-repository https://packages.microsoft.com/ubuntu/18.04/prod
+apt-get update
+
