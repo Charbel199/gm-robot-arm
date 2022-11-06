c803fdc837c3a589f9f4a283504dda144b61275f
Charbel199 <charbel-boumaroun@outlook.com>
2022-11-06 13:42:00 +0100

    Add comments

--- src/utils/cv_utils.py
+++ src/utils/cv_utils.py
@@ -5,18 +5,21 @@ import numpy as np
 import math
 
 
+# Draw bounding boxes around contours
 def draw_contour_bounding_box(image, contours, color=(0, 0, 255), thickness=2):
     for c in contours:
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(image, (x, y), (x + w, y + h), color, thickness)
 
 
+# Get contours (It is better to have thresholded input)
 def get_contours(image):
     cnts = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     return cnts
 
 
+# Concat images and titles in a single window
 def concat_images(images, titles, with_titles=True):
     font = cv2.FONT_HERSHEY_SIMPLEX
     position = (15, 35)
@@ -57,6 +60,7 @@ def concat_images(images, titles, with_titles=True):
     return final_image
 
 
+# Get canny image based on hsv lower and upper bounds
 def get_hsv_canny(image, lower, upper):
     # Extract chess-board lines
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -69,6 +73,7 @@ def get_hsv_canny(image, lower, upper):
     return res1
 
 
+# Get center points of chessboard squares based on canny image
 def get_center_points(canny, lower_area=4500, upper_area=5650, draw_points=False, draw_contours=False, image=None):
     processed_img = cv2.GaussianBlur(canny, (5, 5), 0)
     _, processed_img = cv2.threshold(processed_img, 50, 255, cv2.THRESH_BINARY)
@@ -102,6 +107,7 @@ def get_center_points(canny, lower_area=4500, upper_area=5650, draw_points=False
     return center_points, corner_points, processed_img
 
 
+# Get distance between points
 def get_distance_between_points(point1, point2):
     # d=√((x2 – x1)² + (y2 – y1)²).
     x1, y1 = point1
@@ -109,6 +115,7 @@ def get_distance_between_points(point1, point2):
     return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
 
 
+# Reduce points if close to each other
 def reduce_points(points):
     points_sorted = sorted(points, key=lambda x: (x[0], x[1]))
     groups = []
@@ -128,12 +135,15 @@ def reduce_points(points):
     return points
 
 
+# Chessboard corner points to 2d matrix
 def corner_points_to_2d_matrix(points, size=9):
-    matrix =  [points[i:i + size] for i in range(0, len(points), size)]
+    matrix = [points[i:i + size] for i in range(0, len(points), size)]
     for i, _ in enumerate(matrix):
         matrix[i] = sort_array_of_points(matrix[i])
     return matrix
 
+
+# Cheessboard corner points to squares/corners coordinates
 def corner_points_to_squares(points, size=8, with_text=False, image=None):
     counter = 0
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -159,6 +169,7 @@ def corner_points_to_squares(points, size=8, with_text=False, image=None):
     return squares
 
 
+# Get difference between grayscale images
 def get_images_diff(grayA, grayB, lower_area=1500, upper_area=5650, with_box=False, image=None):
     (score, diff) = compare_ssim(grayA, grayB, full=True)
     diff = (diff * 255).astype("uint8")
@@ -176,8 +187,8 @@ def get_images_diff(grayA, grayB, lower_area=1500, upper_area=5650, with_box=Fal
     return score, diff, thresh, cnts
 
 
+# Get difference between each chessboard square
 def get_each_square_diff(imageA, imageB, squaresA, squaresB, threshold=0.6, show_box=False, image=None):
-
     grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
     grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
     squares_with_differences = []
@@ -191,7 +202,6 @@ def get_each_square_diff(imageA, imageB, squaresA, squaresB, threshold=0.6, show
         x2, y2 = sB[2]
         square2 = grayB[y1:y2, x1:x2]
 
-
         print(f"Comparing {square1.shape} to {square2.shape}")
         if square1.shape != square2.shape:
             up_width = square2.shape[1]
@@ -203,14 +213,15 @@ def get_each_square_diff(imageA, imageB, squaresA, squaresB, threshold=0.6, show
         score, diff, thresh, cnts = get_images_diff(square1, square2, with_box=False, image=None)
         print(f"Score is {score}")
         if score < threshold:
-            cv2.imshow(str(score),square1)
-            cv2.imshow(str(score+1),square2)
+            cv2.imshow(str(score), square1)
+            cv2.imshow(str(score + 1), square2)
             squares_with_differences.append(sA)
             if show_box:
                 cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
     return squares_with_differences
 
 
+# Deduce move made based on square differences
 def get_move_made(squares_with_differences, matrix_2d):
     move_index = []
     for d in squares_with_differences:
@@ -226,6 +237,7 @@ def get_move_made(squares_with_differences, matrix_2d):
     return move_index
 
 
+# Sort array of points based on x then y
 def sort_array_of_points(points):
     points = sorted(points, key=lambda x: x[0])
     points = sorted(points, key=lambda x: x[1])
