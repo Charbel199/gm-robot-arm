e633df25205b0884f2f6d996949360e5aeb978a2
George1044 <Georgedaou_35@hotmail.com>
2022-11-07 01:06:57 +0200

    Add github repo clone for needed SDK and ROS package

--- AzureKinect/DockerBuild.bash
+++ AzureKinect/DockerBuild.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
+git clone https://github.com/microsoft/Azure-Kinect-Sensor-SDK.git
+cd catkin_ws/src
+git clone https://github.com/microsoft/Azure_Kinect_ROS_Driver.git
+cd ../..
 docker build -t azure-kinect-ros .
+rm -rf Azure-Kinect-Sensor-SDK
+rm -rf ./catkin_ws/src/Azure_Kinect_ROS_Driver
