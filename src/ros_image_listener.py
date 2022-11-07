380b45baf760198bd9bea08a263d2467a7dea460
Charbel199 <charbel-boumaroun@outlook.com>
2022-11-07 16:04:25 +0200

    Add ros image listener

---
+++ src/ros_image_listener.py
@@ -0,0 +1,29 @@
+import cv2
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+bridge = CvBridge()
+
+
+def callback(data):
+    try:
+        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+
+    cv2.imshow("Azure Kinect image", cv_image)
+
+    if cv2.waitKey(33) == ord('q'):
+        rospy.signal_shutdown('Manual Shutdow')
+
+
+def listener():
+    rospy.init_node('image_listener')
+    rospy.Subscriber("/rgb/image_raw", Image, callback)
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    listener()
