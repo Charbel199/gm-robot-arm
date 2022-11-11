9d12c3e907e54cad8b18d093f7135672cc5782dd
Charbel199 <charbel-boumaroun@outlook.com>
2022-11-12 01:00:14 +0200

    Add image queue

--- src/cores/vision_core.py
+++ src/cores/vision_core.py
@@ -1,6 +1,8 @@
 import cv2
-from src.utils.cv_utils import concat_images, get_image_information, get_each_square_diff, get_move_made
+from src.utils.cv_utils import concat_images, get_image_information, get_each_square_diff, get_move_made, \
+    get_hsv_filter, get_four_corners
 from src.logger.log import LoggerService
+import numpy as np
 
 logger = LoggerService.get_instance()
 
@@ -10,24 +12,43 @@ class VisionCore:
                  hsv_min_b,
                  hsv_max_b,
                  hsv_min_w,
-                 hsv_max_w):
-        self.current_image = None
-        self.previous_image = None
+                 hsv_max_w,
+                 hsv_min_marker,
+                 hsv_max_marker):
+        self.images = []
+
         self.empty_board_image = None
         self.last_user_move = None
+        self.calibrated_image = None
+
+        self.height, self.width = 600, 600
+        self.image_buffer_size = 6
 
         self.squares = None
+        self.cached_marker_centers = None
         self.matrix_2d = None
 
         self.hsv_min_b = hsv_min_b
         self.hsv_max_b = hsv_max_b
         self.hsv_min_w = hsv_min_w
         self.hsv_max_w = hsv_max_w
+        self.hsv_min_marker = hsv_min_marker
+        self.hsv_max_marker = hsv_max_marker
 
-        empty_image = cv2.imread('src/assets/moves/empty_lichess2.png')
-        imageA = cv2.imread('src/assets/moves/lichess2_1.png')
-        imageB = cv2.imread('src/assets/moves/lichess2_2.png')
-        self.fake_images = [empty_image, imageA, imageB]
+        # empty_image = cv2.imread('src/assets/moves/empty_lichess2.png')
+        # imageA = cv2.imread('src/assets/moves/lichess2_1.png')
+        # imageB = cv2.imread('src/assets/moves/lichess2_2.png')
+        # self.fake_images = [empty_image, imageA, imageB]
+
+        empty_image = cv2.imread('src/assets/moves/real_board/Move_Empty2.jpeg')
+        initial_image = cv2.imread('src/assets/moves/real_board/Move_Initial.jpeg')
+        image1 = cv2.imread('src/assets/moves/real_board/Move1.jpeg')
+        image2 = cv2.imread('src/assets/moves/real_board/Move2.jpeg')
+        image3 = cv2.imread('src/assets/moves/real_board/Move3.jpeg')
+        image4 = cv2.imread('src/assets/moves/real_board/Move4.jpeg')
+        image5 = cv2.imread('src/assets/moves/real_board/Move5.jpeg')
+
+        self.fake_images = [empty_image, initial_image, image1, image2, image3, image4, image5]
 
         self.chessboard_map = [['a8', 'a7', 'a6', 'a5', 'a4', 'a3', 'a2', 'a1'],
                                ['b8', 'b7', 'b6', 'b5', 'b4', 'b3', 'b2', 'b1'],
@@ -71,16 +92,19 @@ class VisionCore:
     def capture_image(self):
         logger.info("Capturing image")
         image = None  # TODO: Get from camera
-        return self.fake_images.pop(0)
+        image = self.fake_images.pop(0)
+        image = self.preprocess_image(image)
+        return image
 
     def update_images(self):
         logger.info("Updating images")
-        self.previous_image = self.current_image
-        self.current_image = self.capture_image()
+        if len(self.images) > self.image_buffer_size:
+            self.images.pop(0)
+        self.images.append(self.capture_image())
 
     def capture_initial_chessboard_layout(self):
         logger.info("Capturing initial board layout")
-        self.current_image = self.capture_image()  # Called when pieces are set
+        self.update_images()  # Called when pieces are set
         logger.info("Done capturing initial board layout")
 
     def calibrate(self):
@@ -90,24 +114,25 @@ class VisionCore:
 
         self.squares, self.matrix_2d, calibration_b, calibration_w, calibration_bw, calibration_processed = get_image_information(
             self.empty_board_image, image_write, self.hsv_min_b, self.hsv_max_b,
-            self.hsv_min_w, self.hsv_max_w)
+            self.hsv_min_w, self.hsv_max_w, board_percentage=0.7)
+        self.calibrated_image = image_write
         logger.info("Done calibrating board")
 
     def get_user_move(self) -> str:
         logger.info("Detecting user move")
-        if self.current_image.shape != self.previous_image.shape:
+        if self.images[-1].shape != self.images[-2].shape:
             # TODO: Warp and crop
-            logger.debug(f"Reshaping self.current_image from {self.current_image.shape} to {self.previous_image.shape}")
-            up_width = self.current_image.shape[1]
-            up_height = self.current_image.shape[0]
+            logger.debug(f"Reshaping self.current_image from {self.images[-1].shape} to {self.images[-2].shape}")
+            up_width = self.images[-1].shape[1]
+            up_height = self.images[-1].shape[0]
             up_points = (up_width, up_height)
-            self.previous_image = cv2.resize(self.previous_image, up_points, interpolation=cv2.INTER_LINEAR)
+            self.images[-2] = cv2.resize(self.images[-2], up_points, interpolation=cv2.INTER_LINEAR)
 
         # Difference between snapshots
-        squares_with_differences, squares_differences_images = get_each_square_diff(self.previous_image,
-                                                                                    self.current_image,
+        squares_with_differences, squares_differences_images = get_each_square_diff(self.images[-2],
+                                                                                    self.images[-1],
                                                                                     self.squares,
-                                                                                    threshold=0.8,
+                                                                                    threshold=0.4,
                                                                                     show_box=True,
                                                                                     # image=image_write
                                                                                     )
