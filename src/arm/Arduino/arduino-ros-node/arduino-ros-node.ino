4399e41ba40835007d979be56d7fc62da19f8bed
George1044 <Georgedaou_35@hotmail.com>
2022-11-18 16:21:29 +0200

    Fix bug in arduino code

--- src/arm/Arduino/arduino-ros-node/arduino-ros-node.ino
+++ src/arm/Arduino/arduino-ros-node/arduino-ros-node.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 #include <ros.h>
 #include <rosserial_msgs/ServoPositions.h>
+#include <std_msgs/Bool.h>
 
 #define servoPin1 7
 #define servoPin2 2
@@ -29,6 +30,7 @@ Servo servo6;   //Range: 90 to 180
 
 
 ros::NodeHandle node;
+std_msgs::Bool bool_msg;
 
 void message(const rosserial_msgs::ServoPositions& servo_positions){
   int servo1_des_pos = servo_positions.servo1;
@@ -45,26 +47,27 @@ void message(const rosserial_msgs::ServoPositions& servo_positions){
   float servo6_pos = servo6.read();
 
   if(abs(servo1_des_pos - servo1_pos) > EPSILON){
-    moveServo(servo1, servo1_pos, servo1_des_pos, DELAY)
+    moveServo(servo1, servo1_pos, servo1_des_pos, DELAY);
     }
   if(abs(servo2_des_pos - servo2_pos) > EPSILON){
-    moveServo(servo2, servo2_pos, servo2_des_pos, DELAY)
+    moveServo(servo2, servo2_pos, servo2_des_pos, DELAY);
     }
   if(abs(servo3_des_pos - servo3_pos) > EPSILON){
-    moveServo(servo3, servo3_pos, servo3_des_pos, DELAY)
+    moveServo(servo3, servo3_pos, servo3_des_pos, DELAY);
     }
   if(abs(servo4_des_pos - servo4_pos) > EPSILON){
-    moveServo(servo4, servo4_pos, servo4_des_pos, DELAY)
+    moveServo(servo4, servo4_pos, servo4_des_pos, DELAY);
     }
   if(abs(servo5_des_pos - servo5_pos) > EPSILON){
-    moveServo(servo5, servo5_pos, servo5_des_pos, DELAY)
+    moveServo(servo5, servo5_pos, servo5_des_pos, DELAY);
     }
   if(abs(servo6_des_pos - servo6_pos) > EPSILON){
-    moveServo(servo6, servo6_pos, servo6_des_pos, DELAY)
+    moveServo(servo6, servo6_pos, servo6_des_pos, DELAY);
     }
     
   }
 ros::Subscriber<rosserial_msgs::ServoPositions> controller("/control/arm", &message);
+ros::Publisher pub("/control/move_done", &bool_msg);
  
 void setup() { 
   Serial.begin(9600);
@@ -75,6 +78,8 @@ void setup() {
   servo5.attach(servoPin5);
   servo6.attach(servoPin6);
   node.initNode();
+  node.advertise(pub);
+  bool_msg.data = true;
   node.subscribe(controller);
   goToInitialPose();
 } 
@@ -111,4 +116,6 @@ void moveServo(Servo servoX, int from, int to, int delayValue){
     delay(delayValue);
   }
   }
+
+  pub.publish(&bool_msg);
 }
