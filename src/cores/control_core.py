e322b1de1c566232f9b8564888614380d92c4b9a
Charbel199 <charbel-boumaroun@outlook.com>
2022-12-15 13:21:53 +0200

    Add many move sequences

--- src/cores/control_core.py
+++ src/cores/control_core.py
@@ -1,14 +1,15 @@
- #!/usr/bin/env python
+# !/usr/bin/env python
 import rospy
 from rosserial_msgs.msg import ServoPositions
 from rosserial_msgs.msg import Moves
 from std_msgs.msg import Bool
 from logger.log import LoggerService
 import time
-from utils.move_utils import MOVE_DICT, SAFE_POSE, YEET_POSE
+from utils.move_utils import MOVE_DICT, SAFE_POSE, YEET_POSE, MANY_MOVES_DICT, YEET_POSE_SEQUENCE
+
 
 class ControlCore:
-    
+
     def __init__(self):
         try:
             print(f'Launching Control Core')
@@ -24,19 +25,19 @@ class ControlCore:
             rospy.spin()
         except rospy.ROSInterruptException:
             pass
-   
-   #move tuple example: (("a5", "PICK"),("a5", "YEET") ("a1", "PICK"), ("a5", "PLACE"))
+
+    # move tuple example: (("a5", "PICK"),("a5", "YEET") ("a1", "PICK"), ("a5", "PLACE"))
 
     def move_done(self, done):
         self.done = done.data
         print(f"Setting move done to {self.done}")
 
     def wait_for_move(self):
-        while self.done==False:
-           pass
+        while self.done == False:
+            pass
         self.rate.sleep()
         self.done = False
-        #time.sleep(3)
+        # time.sleep(3)
         pass
 
     def go_to_safe_pose(self):
@@ -45,46 +46,56 @@ class ControlCore:
 
     def move_sequence(self, control_move):
         for i in range(6):
-            if i==0:
-                self.pub.publish(self.GRIPPER_CLOSE if control_move[0]==self.GRIPPER_OPEN else self.GRIPPER_OPEN, SAFE_POSE[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
+            if i == 0:
+                self.pub.publish(self.GRIPPER_CLOSE if control_move[0] == self.GRIPPER_OPEN else self.GRIPPER_OPEN,
+                                 SAFE_POSE[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
                 self.wait_for_move()
                 print(f"Sequence {i} completed")
-            if i==1:
-                self.pub.publish(self.GRIPPER_CLOSE if control_move[0]==self.GRIPPER_OPEN else self.GRIPPER_OPEN, control_move[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
+            if i == 1:
+                self.pub.publish(self.GRIPPER_CLOSE if control_move[0] == self.GRIPPER_OPEN else self.GRIPPER_OPEN,
+                                 control_move[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
                 self.wait_for_move()
                 print(f"Sequence {i} completed")
-            if i==2:
-                self.pub.publish(self.GRIPPER_CLOSE if control_move[0]==self.GRIPPER_OPEN else self.GRIPPER_OPEN, control_move[1], SAFE_POSE[2], control_move[3], SAFE_POSE[4], control_move[5])
+            if i == 2:
+                self.pub.publish(self.GRIPPER_CLOSE if control_move[0] == self.GRIPPER_OPEN else self.GRIPPER_OPEN,
+                                 control_move[1], SAFE_POSE[2], control_move[3], SAFE_POSE[4], control_move[5])
                 self.wait_for_move()
                 print(f"Sequence {i} completed")
-            if i==3:
-                self.pub.publish(self.GRIPPER_CLOSE if control_move[0]==self.GRIPPER_OPEN else self.GRIPPER_OPEN, control_move[1], control_move[2], control_move[3], SAFE_POSE[4], control_move[5])
+            if i == 3:
+                self.pub.publish(self.GRIPPER_CLOSE if control_move[0] == self.GRIPPER_OPEN else self.GRIPPER_OPEN,
+                                 control_move[1], control_move[2], control_move[3], SAFE_POSE[4], control_move[5])
                 self.wait_for_move()
                 print(f"Sequence {i} completed")
-            if i==4:
-                self.pub.publish(self.GRIPPER_CLOSE if control_move[0]==self.GRIPPER_OPEN else self.GRIPPER_OPEN, control_move[1], control_move[2], control_move[3], control_move[4], control_move[5])
+            if i == 4:
+                self.pub.publish(self.GRIPPER_CLOSE if control_move[0] == self.GRIPPER_OPEN else self.GRIPPER_OPEN,
+                                 control_move[1], control_move[2], control_move[3], control_move[4], control_move[5])
                 self.wait_for_move()
                 print(f"Sequence {i} completed")
-            
-            if i==5:
-                self.pub.publish(control_move[0], control_move[1], control_move[2], control_move[3], control_move[4], control_move[5])
+
+            if i == 5:
+                self.pub.publish(control_move[0], control_move[1], control_move[2], control_move[3], control_move[4],
+                                 control_move[5])
                 self.wait_for_move()
                 print(f"Sequence {i} completed")
 
         for i in range(5):
-            if i==0:
-                self.pub.publish(control_move[0], control_move[1], control_move[2], control_move[3], SAFE_POSE[4], control_move[5])
+            if i == 0:
+                self.pub.publish(control_move[0], control_move[1], control_move[2], control_move[3], SAFE_POSE[4],
+                                 control_move[5])
                 self.wait_for_move()
-            if i==1:
-                self.pub.publish(control_move[0], control_move[1], control_move[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
+            if i == 1:
+                self.pub.publish(control_move[0], control_move[1], control_move[2], SAFE_POSE[3], SAFE_POSE[4],
+                                 control_move[5])
                 self.wait_for_move()
-            if i==2:
-                self.pub.publish(control_move[0], control_move[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
+            if i == 2:
+                self.pub.publish(control_move[0], control_move[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4],
+                                 control_move[5])
                 self.wait_for_move()
-            if i==3:
-                self.pub.publish(control_move[0], SAFE_POSE[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], control_move[5])
+            if i == 3:
+                self.pub.publish(control_move[0], SAFE_POSE[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4],
+                                 control_move[5])
                 self.wait_for_move()
-            if i==4:
+            if i == 4:
                 self.pub.publish(control_move[0], SAFE_POSE[1], SAFE_POSE[2], SAFE_POSE[3], SAFE_POSE[4], SAFE_POSE[5])
                 self.wait_for_move()
             '''
@@ -105,7 +116,45 @@ class ControlCore:
                 self.wait_for_move()
             '''
         current_counter = rospy.get_param('/control/move_complete_counter')
-        rospy.set_param('/control/move_complete_counter', current_counter-1)
+        rospy.set_param('/control/move_complete_counter', current_counter - 1)
+
+    def many_move_sequence(self, control_moves):
+        for i, control_move in enumerate(control_moves):
+            if i == len(control_moves)-1:
+                self.pub.publish(control_move[0], control_move[1], control_move[2], control_move[3], control_move[4],
+                                 control_move[5])
+            else:
+                self.pub.publish(self.GRIPPER_CLOSE if control_move[0] == self.GRIPPER_OPEN else self.GRIPPER_OPEN, control_move[1], control_move[2], control_move[3], control_move[4],
+                             control_move[5])
+
+            self.wait_for_move()
+        print(f"First sequence completed")
+        control_moves.reverse()
+        for control_move in control_moves:
+            self.pub.publish(control_move[0], control_move[1], control_move[2], control_move[3], control_move[4],
+                             control_move[5])
+            self.wait_for_move()
+        print(f"Second sequence completed")
+        current_counter = rospy.get_param('/control/move_complete_counter')
+        rospy.set_param('/control/move_complete_counter', current_counter - 1)
+
+    def send_many_moves(self, move):
+        print(f"RECEIVED SEND MOVE {move.type} TO {move.square}")
+        move_type = move.type
+        if move_type == 'YEET':
+            control_moves = YEET_POSE_SEQUENCE
+        else:
+            control_moves = MANY_MOVES_DICT[move.square].copy()
+            print(f"List of moves {control_moves}")
+
+        if move_type == 'PICK':
+            control_moves = [move.insert(0, self.GRIPPER_CLOSE) for move in control_moves]
+        else:
+            control_moves = [move.insert(0, self.GRIPPER_OPEN) for move in control_moves]
+
+        self.many_move_sequence(control_moves)
+        print(f"Executed move.")
+        pass
 
     def send_move(self, move):
         print(f"RECEIVED SEND MOVE {move.type} TO {move.square}")
@@ -116,16 +165,15 @@ class ControlCore:
             control_move = MOVE_DICT[move.square].copy()
             print(f"BEFORE INSERT {control_move}")
         if move_type == 'PICK':
-            control_move.insert(0,self.GRIPPER_CLOSE)
+            control_move.insert(0, self.GRIPPER_CLOSE)
         else:
-            control_move.insert(0,self.GRIPPER_OPEN)
-        
+            control_move.insert(0, self.GRIPPER_OPEN)
+
         print(f"AFTER INSERT Control move: {control_move}")
-        self.move_sequence(control_move)           
+        self.move_sequence(control_move)
         print(f"Executed move.")
         pass
 
-    
 
 if __name__ == "__main__":
     controller = ControlCore()
