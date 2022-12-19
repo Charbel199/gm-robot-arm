5b3702942766caa621e3f925ca19ec2f7cab12a6
Charbel199 <charbel-boumaroun@outlook.com>
2022-12-19 20:19:44 +0200

    Add comments to main env variables

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import sys
 
 sys.path.append('../')
 
-USE_CAMERA = int(os.getenv("USE_CAMERA", "0"))
-IS_SIMULATION = int(os.getenv("IS_SIMULATION", "1"))
-USE_ROBOT = int(os.getenv("USE_ROBOT", "0"))
-WITH_SOUND = int(os.getenv("WITH_SOUND", "0"))
-USE_PREVIOUS_CALIBRATED_BOARD = int(os.getenv("USE_PREVIOUS_CALIBRATED_BOARD", "0"))
-ENGINE_SIDE = os.getenv("ENGINE_SIDE", "BLACK")
+USE_CAMERA = int(os.getenv("USE_CAMERA", "0")) # 1: Use camera / 0: Use Fake images in Vision Core
+IS_SIMULATION = int(os.getenv("IS_SIMULATION", "1")) # 1: Use Fake moves in Chess core / 0: Use engine next best move
+USE_ROBOT = int(os.getenv("USE_ROBOT", "0")) # 1: Call robot arm to perform move / 0: Do not call robot arm
+WITH_SOUND = int(os.getenv("WITH_SOUND", "0")) # 1: Enable sound on turn switch / 0: Disable sound on turn switch
+USE_PREVIOUS_CALIBRATED_BOARD = int(os.getenv("USE_PREVIOUS_CALIBRATED_BOARD", "0")) # 1: Use previously recorded empty board for calibration
+ENGINE_SIDE = os.getenv("ENGINE_SIDE", "BLACK") # Engine Side: Black or White
 
 
 core = GMCore(engine_side=ENGINE_SIDE, use_camera=USE_CAMERA, is_simulation=IS_SIMULATION, with_sound=WITH_SOUND,
